b88ce96f6dbdd8c71f523c393f70a62b182f279d
Hallstein Brøtan <Hallstein.Brotan@nrk.no>
2019-04-09 08:24:33 +0200

    remove lodash

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-var _ = require('lodash');
 
 // AZURE SQL DATABASE CONNECTION USING SERIATE
 // ===================================================================================
