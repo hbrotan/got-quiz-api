c520aee1ceca669871e17b43a1f05449acd8bdde
Hallstein Brøtan <Hallstein.Brotan@nrk.no>
2019-04-24 17:47:13 +0200

    fix sql error

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ router.route('/answers')
 	.get(function(req, res){
 		sql.execute({  
 			query: 
-				'SELECT [Name],[Q1],[Q2],[Q3],[Q4],[Q5],[Q6],[Q7],[Q8],[Q9],[Q10],[Q11],[Q12],[Q13],[Q14],[Q15],[Q16],[Q17],[Q18],[Q19],[Q20],[Q21],[Q22],[Q23],[Q24],[Q25],[Q26],[Q27],[Q28],[Q29],[Q30] ' + 
+				'SELECT [Name],[Q1],[Q2],[Q3],[Q4],[Q5],[Q6],[Q7],[Q8],[Q9],[Q10],[Q11],[Q12],[Q13],[Q14],[Q15],[Q16],[Q17],[Q18],[Q19],[Q20],[Q21],[Q22],[Q23],[Q24],[Q25],[Q26],[Q27],[Q28],[Q29],[Q30], ' + 
 					'(CASE [Q1] WHEN 'L' THEN 2 ELSE 0 END + ' +
 					'CASE [Q2] WHEN 'L' THEN 2 ELSE 0 END + ' +
 					'CASE [Q3] WHEN 'L' THEN 2 ELSE 0 END + ' +
